9d86efe4c90b7f0e14bd5fb889ca004b9698ac6f
Adam Donatelli <adamdonatelli@Adams-Air.frontierlocal.net>
2019-07-07 12:43:17 -0400

    this may not be a good copy. Go to one more commit before this

--- routes/profile.js
+++ routes/profile.js
@@ -10,7 +10,7 @@ const User = require('../models/user');
 // @access  Private
 router.get('/me', auth, async (req, res) => {
     try{
-        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+          const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
 
         if(!profile){
             return res.status(400).json({msg: 'There is no profile for this user'});
